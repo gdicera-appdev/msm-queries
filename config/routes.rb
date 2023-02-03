9198419a94a8a91301bfc8f3678e6b968ef3acba
gdicera <giuseppedicera@uchicago.edu>
2023-02-03 00:35:54 +0000

    created all 4 actor, director, character, and movie

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   get("/", { :controller => "application", :action => "homepage" })
+  get("/directors", { :controller => "directors", :action => "index" })
+  get("/actors", {:controller => "actors", :action => "index" })
+  get("/characters", {:controller => "characters", :action => "index"}
+  get("/movies", {:controller => "movies", :action => "index"})
 end
