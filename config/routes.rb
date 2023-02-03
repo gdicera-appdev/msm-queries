6b19088376d6763af4a2db9d8ab94f4fc6a6298c
gdicera <giuseppedicera@uchicago.edu>
2023-02-03 01:46:00 +0000

    made progress on directors

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
+
+  get("/directors/eldest", { :controller => "directors", :action => "wisest" })
+  get("/directors/youngest", { :controller => "directors", :action => "youngest" })
+
+  get("/directors/:an_id", { :controller => "directors", :action => "director_details" })
+
   get("/", { :controller => "application", :action => "homepage" })
   get("/directors", { :controller => "directors", :action => "index" })
+
+
   get("/actors", {:controller => "actors", :action => "index" })
-  get("/characters", {:controller => "characters", :action => "index"}
+  get("/characters", {:controller => "characters", :action => "index"})
   get("/movies", {:controller => "movies", :action => "index"})
 end
